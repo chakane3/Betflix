35174c4b290970fe73696585e990de6b7fd9c6b7
Chakane Shegog <chakanezshegog@gmail.com>
2019-10-23 17:31:11 -0400

    Initial Commit

---
+++ Betflix/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Betflix
+//
+//  Created by Chakane Shegog on 10/18/19.
+//  Copyright © 2019 Chakane Shegog. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Betflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Betflix
+//
+//  Created by Chakane Shegog on 10/17/19.
+//  Copyright © 2019 Chakane Shegog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
